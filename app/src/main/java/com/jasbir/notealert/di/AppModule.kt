bc7a71cccbc396c5dd5a8fa28dfe861df07f032d
Jasbir <jasbirh1996@gmail.com>
2023-06-16 06:20:56 +0530

    app completed

--- app/src/main/java/com/jasbir/notealert/di/AppModule.kt
+++ app/src/main/java/com/jasbir/notealert/di/AppModule.kt
@@ -1,11 +1,49 @@
 package com.jasbir.notealert.di
 
+import android.app.Application
+import androidx.room.Room
+import com.jasbir.notealert.feature_note.data.local_db.NoteDao
+import com.jasbir.notealert.feature_note.data.local_db.NoteDb
+import com.jasbir.notealert.feature_note.data.repository.NoteRepositoryImpl
+import com.jasbir.notealert.feature_note.domain.repository.NoteRepository
+import com.jasbir.notealert.feature_note.domain.use_case.AddNoteUseCase
+import com.jasbir.notealert.feature_note.domain.use_case.DeleteNoteUseCase
+import com.jasbir.notealert.feature_note.domain.use_case.GetNoteByIdUseCase
+import com.jasbir.notealert.feature_note.domain.use_case.GetNotesUseCase
+import com.jasbir.notealert.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDb {
+        return Room.databaseBuilder(
+            app,
+            NoteDb::class.java,
+            NoteDb.DATABASE_NAME
+        ).build()
+    }
 
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDb): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotesUseCase(repository),
+            deleteNote = DeleteNoteUseCase(repository),
+            addNote = AddNoteUseCase(repository),
+            getNoteById = GetNoteByIdUseCase(repository)
+        )
+    }
 }
--- app/src/main/java/com/jasbir/notealert/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/jasbir/notealert/feature_note/presentation/MainActivity.kt
@@ -1,46 +1,84 @@
 package com.jasbir.notealert.feature_note.presentation
 
+import AddEditNoteScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jasbir.notealert.ui.theme.NoteAlertTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
+import com.jasbir.notealert.feature_note.presentation.notes.NotesScreen
+import com.jasbir.notealert.ui.theme.NoteAlertTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NoteAlertTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NotesScreen.route
+                    ) {
+
+
+                        composable(route = Screen.NotesScreen.route) {
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route +
+                                    "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                            )
+                        ) {
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(
+                                navController = navController,
+                                noteColor = color
+                            )
+                        }
+                    }
+                }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteAlertTheme {
-        Greeting("Android")
-    }
-}
